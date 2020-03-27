fc512530e880fc84df6d9bb816b255a111181ac8
Richard Magnotti <richardmagnotti@Richards-MacBook-Pro.local>
2020-03-27 04:06:09 -0400

    final commit, roughly working hough transform

--- Problem2.py
+++ Problem2.py
@@ -7,93 +7,76 @@ import copy
 This function is to outline circles belonging to each r value and the corresponding local max (a,b) values
 '''
 def add_max(B, img):
-    # img_width = img.shape[1]
-    # for r in range(1, 20):
-    #     for ab in B:
-    #         #get a value
-    #         a = int(float(ab.split(',')[0]))
-    #         #get b value
-    #         b = int(float(ab.split(',')[1]))
-    #         print('ab', a,b)
-    #
-    #         center_coordinates = (a, b)
-    #         color = (255, 0, 0) #BGR, want blue circles
-    #         thickness = 1
-    #
-    #         cv2.circle(img, center_coordinates, r, color, thickness) #should work as canvas
-    #
-    # cv2.imshow('circled!', img)
-    # cv2.waitKey(0)
-    #
-    # return img
-
-    for key in B.keys():
-        #get r value
-        r = int(float(key.split(',')[0]))
-        #get a value
-        a = int(float(key.split(',')[1]))
-        #get b value
-        b = int(float(key.split(',')[2]))
-        print('rab',r, a,b)
-
-        center_coordinates = (a, b)
+    for rab in B:
+        center_coordinates = (rab[1], rab[2])
         color = (255, 0, 0) #BGR, want blue circles
         thickness = 1
 
-        cv2.circle(img, center_coordinates, r, color, thickness) #should work as canvas
+        cv2.circle(img, center_coordinates, rab[0], color, thickness) #should work as canvas
 
     cv2.imshow('circled!', img)
     cv2.waitKey(0)
 
     return img
 '''
-The following function finds the local maxima/highest accumulated (a,b)-values, top 5 overall
+The following function finds the local maxima/highest accumulated (a,b)-values
+
+Our accumulator matrix is 3D, so we iterate over each element in the matrix and find the local maxima by querying
+each element's neighborhood within the layer
 '''
-def findMax(A, width):
-    # '''
-    #     1-choose an r value, access the corresponding dictionary
-    #     2-find top 10 highest/local max counts per (a,b) combination
-    #     3-find top 10 local max counts overall
-    # '''
-    # #find top votes per r-value, then delete key value pair and find next top value:
-    # #   repeat for 5
-    # B = []
-    # for count in range(5): #top 5 vote-(a,b) pairs
-    #     max_ab = max(A.values())
-    #     print('top value ', max_ab)
-    #     for key in A.keys():
-    #         if A[key] == max_ab:
-    #             B.append(key)
-    #             del A[key] #delete key value pair
-    #             break
+def findMax(A):
+    max_list = []
+    for r in range(A.shape[0]):
+        for a in range(A.shape[1]):
+            for b in range(A.shape[2]):
+                max_val = np.max(A[r, a: a + 20, b: b + 20])
+                max_list.append(max_val)
+    max_list = list(set(max_list))
 
-    #A = {r1: {(a1,b1):votes, (a2,b2):votes, ...}, r2: {}, ...}
-    r_max_dict = {}
-    for r in range(1, 20):
-        '''
-            1-choose an r value, access the corresponding dictionary
-            2-find top 10 highest/local max counts per (a,b) combination
-            3-find top 10 local max counts overall
-        '''
-        #find top votes per r-value, then delete key value pair and find next top value:
-        #   repeat for 10
-        for count in range(10): #top 10 vote-(a,b) pairs
-            max_ab = max(A[r].values())
-            for key in A[r].keys(): #keys are (a,b) value combinations
-                if A[r][key] == max_ab:
-                    max_key = str(r) + ',' + key
-                    r_max_dict[max_key] = A[r][key]
-                    del A[r][key] #delete key value pair
-                    break
+    '''
+    we choose our final max values based on whether or not the value is above the 9xth percentile,
+        indicating it is very likely true circle. 
+    '''
+    max_fin = []
+    Q3 = np.percentile(max_list, 85, interpolation='midpoint')
+    for value in max_list:
+        if value > Q3:
+            max_fin.append(value)
 
-    #r_max_dict = {'r,a,b' = votes, ...}
-    while (len(r_max_dict) > 5):
-        del r_max_dict[min(r_max_dict, key=r_max_dict.get)]
+    '''
+    Now we want to make a list of tuples containing the indices corresponding to the highest voted (r, a, b) values
+    in the accumulator matrix
+    '''
+    index_list = []
+    for element in max_fin:
+        r, a, b = np.where(A == element)
+        index_list.append((r[0], a[0], b[0]))
+    print(index_list)
 
-    return r_max_dict
+    '''
+    Now we do a soft remove of any similar indices in case the bin sizes are not sufficient for filtering alike elements
+    '''
+    index_list = list(index_list)
+    new_index_list = copy.deepcopy(index_list)
+    print('before ', index_list)
+
+    for i in range(len(new_index_list)):
+        for j in range(i, len(new_index_list)):
+            diff = np.subtract(new_index_list[i], new_index_list[j])
+            diff[diff <= 25] = 0
+
+            #we only need one of the similar values, so delete the other
+            if 0 in diff:
+                try:
+                    index_list.remove(index_list[j])
+                except:
+                    continue
+
+    print('after ', index_list)
+    return index_list
 
 '''
-The following function is to memoize the degree-to-radian calculations, as to not need to recompute it each time
+The  following function is to memoize the degree-to-radian calculations, as to not need to recompute it each time
     on the fly. This will reduce the calculation time to O(1) constant-time lookup
     
 3 hash tables/dicts are used:
@@ -117,7 +100,7 @@ def preFill_radians():
     return cos_calcs, sin_calcs
 
 #gradient is undefined at borders of image
-def gradient_direction(x,y, img):
+def gradient_direction(x, y, img):
     if x == img.shape[1]: #in case at border
         x_grad = 0 #if at right edge point
     else:
@@ -133,7 +116,7 @@ def gradient_direction(x,y, img):
 
     return g_direction
 
-def hough(edges):
+def hough(edges, a_bin_list, b_bin_list):
     '''
     To generate all possible circle origins corresponding to one (x,y) edge point in the original image:
         1) take a detected edge point from image space
@@ -144,40 +127,41 @@ def hough(edges):
                 a = x-cos(theta), b = y-sin(theta), iterate over all theta
         Repeat steps 1-3 until we have iterated over all pixels
     '''
-    A = {}  # accumulator 'matrix' (dictionary)
     img_width = edges.shape[1] #get the width of the image
     img_height = edges.shape[0]
+
+    #get diagonal length of image
+    img_diag = np.int(np.floor(np.sqrt(np.square(img_height) + np.square(img_width))))
     cos, sin = preFill_radians()
-    for r in range(1, 20):
-        sub_accumulator = {}
+    #accumulator
+    A = np.zeros((50, img_width, img_height), dtype=int)
+    print('size here ', A.shape)
+
+    #make all data into 3d array for binning, matrix(r, a, b)
+    #accumulator = np.zeros((20, img_height, img_width), dtype=int)
+    for r in range(1, 50):
         print('r value', r)
         for x in range(edges.shape[0]):
             for y in range(edges.shape[1]):
                 if edges[x][y] > 0: #if current point's value is > 0, it is an edge point
-                    # grad_xy = gradient_direction(x, y, edges)
                     for theta in range(0, 361):
-                        a = np.ceil(x-(np.ceil(r*cos[theta]))) #just a table lookup and minor calculation
-                        b = np.ceil(y-(np.ceil(r*sin[theta]))) #just a table lookup and minor calculation
+                        a = x-(r*cos[theta]) #just a table lookup and minor calculation
+                        b = y-(r*sin[theta]) #just a table lookup and minor calculation
 
-                        # get rid of any possible points that do not point in direction of current points gradient
-                        # if gradient_direction(a, b, edges) != grad_xy:
-                        #     break
                         #conitinue to next loop without storing ab values, because if a or b is negative,
                         #the circle origin pixel locations are from off-screen/out of frame
                         if a < 0 or b < 0:
                             continue
 
-                        ab_str = str(a)+','+str(b)
-                        #print(ab_str)
+                        #find the closest value from the bin list
+                        a_bin_val = min(a_bin_list, key=lambda x: abs(x - a))
+                        b_bin_val = min(b_bin_list, key=lambda x: abs(x - b))
 
-                        if ab_str in sub_accumulator.keys():
-                            sub_accumulator[ab_str] += 1
-                        else:
-                            sub_accumulator[ab_str] = 1
+                        A[r, a_bin_val, b_bin_val] += 1
                 else:
                     continue #cont. to next iteration of current loop
-        A[r] = sub_accumulator
     return A
+
 def main():
     #load the image
     img = cv2.imread('/Users/richardmagnotti/PycharmProjects/MVHW3/csc249tracking/ueCwZ.png')
@@ -194,15 +178,26 @@ def main():
     cv2.imshow('imgCanny', edges)
     cv2.waitKey(0)
 
+    #generate a and b bins
+    a_bin_list = []
+    b_bin_list = []
+    for i in range(0, img.shape[1], 50):
+        a_bin_list.append(i)
+
+    for i in range(0, img.shape[0], 50):
+        b_bin_list.append(i)
+
     #perform Hough transform
-    accum = hough(edges)
+    accum = hough(edges, a_bin_list, b_bin_list)
     print('acum', accum)
 
     #now find max (a,b)
-    max_list = findMax(accum, img.shape[1])
+    max_list = findMax(accum)
     print('max list', max_list)
 
     #lastly we want to add the circles corresponding to the found (a,b)
     fin_img = add_max(max_list, img)
+
+    cv2.waitKey(0)
 
 main()
